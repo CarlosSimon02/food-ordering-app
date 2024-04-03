ecf17a5cf451b84720e452135f51f3258eceb108
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-03 20:18:09 +0800

    backend connect to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,19 @@
-import express, {Request,Response} from "express";
-import cors from 'cors';
-import 'dotenv/config';
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to database"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/text",async (req: Request, res: Response) => {
-  res.json({message: "Hello"})
-})
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
-  console.log("server started on localhost:7000")
+  console.log("server started on localhost:7000");
 });
