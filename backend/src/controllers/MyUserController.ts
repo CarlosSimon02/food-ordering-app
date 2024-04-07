7196b0f4cc027cdf495084482f3b4c7249974f42
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-07 21:26:03 +0800

    corrected spelling

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -11,7 +11,7 @@ const getCurrentUser = async (req: Request, res: Response) => {
     res.json(currentUser);
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ message: "Semething went wrong" });
+    return res.status(500).json({ message: "Something went wrong" });
   }
 };
 
