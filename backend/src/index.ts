8e78034efca6ae25e4b8cbb0f83298d5ddb35fe2
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-09 09:54:28 +0800

    add routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import restaurantRoute from "./routes/RestaurantRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => console.log("connected to database"));
+  .then(() => console.log("Connected to database!"));
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -19,8 +19,12 @@ cloudinary.config({
 });
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
 
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
