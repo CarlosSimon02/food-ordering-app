ad77788a63d455433ad56a0f048026c2e24aec95
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-09 09:55:17 +0800

    add order route

--- backend/src/routes/OrderRoute.ts
+++ backend/src/routes/OrderRoute.ts
@@ -4,11 +4,15 @@ import { jwtCheck, jwtParse } from "../middleware/auth";
 
 const router = express.Router();
 
+router.get("/", jwtCheck, jwtParse, OrderController.getMyOrders);
+
 router.post(
   "/checkout/create-checkout-session",
   jwtCheck,
   jwtParse,
   OrderController.createCheckoutSession
 );
+
+router.post("/checkout/webhook", OrderController.stripeWebhookHandler);
 
 export default router;
